25ddf63daa89f87308c5ff618499ff4d84518586
amq-tutti <tutti-amq@proton.me>
2026-05-03 22:26:15 +0300

    Add si-vis-pr song list and update config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    localStoragePrefix: "party-rank-sorter",
+    localStoragePrefix: "si-vis-pr",
     title: "Party Rank Sorter",
     description: "Party rank sorter for your custom list of songs."
 };
